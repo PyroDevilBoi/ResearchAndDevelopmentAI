c6586246dce506712306872c6c032412fd3cfb60
AT228641 <AT228641@falmouth.ac.uk>
2021-11-22 00:30:09 +0000

    +Initialized movement scripts.

---
+++ New Unity Project/Assets/Scripts/NoveltySearch/Movement/Abstracts/AbstractAgentController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AgentScripts.Agent.Abstract
+{
+
+    [RequireComponent(typeof(MovementController))]
+    public abstract class AbstractAgentController : MonoBehaviour
+    {
+        [SerializeField] protected ScriptableObjects.Containers.FloatReference _movingSpeed;
+        [SerializeField] protected ScriptableObjects.Containers.FloatReference _rotationSpeed;
+
+        public void Tick()
+        {
+            DoAction();
+        }
+
+        protected abstract void DoAction();
+
+        public abstract void Reset(Vector3 defaultPos);
+    }
+}
---
+++ New Unity Project/Assets/Scripts/NoveltySearch/Movement/Abstracts/AbstractAgentMovementController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace AgentScripts.Agent.Abstract
+{
+    public abstract class AbstractAgentMovementController : MonoBehaviour
+    {
+        public abstract void Move(Vector3 vector);
+
+        public abstract void Rotate(Vector3 vector);
+    }
+}
